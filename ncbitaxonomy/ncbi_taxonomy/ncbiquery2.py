dfaee4b429cc487b70d0c162731cc40b88ddfd40
Amber Wright <amber.wright@nanoporetech.com>
2021-05-04 17:49:23 +0100

    Resolve CW-52 "Taxonomy helpers"

---
+++ ncbitaxonomy/ncbi_taxonomy/ncbiquery2.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from . import ncbiquery
+
+
+RANKS = [
+    "superkingdom",
+    "kingdom",
+    "phylum",
+    "class",
+    "order",
+    "family",
+    "genus",
+    "species",
+]
+
+
+class NCBITaxa(ncbiquery.NCBITaxa):
+
+    def get_ranked_lineage(self, taxid):
+        """
+        Wrapper around self.get_lineage
+        """
+        if not taxid:
+            return {}
+        try:
+            lineage = self.get_rank(super().get_lineage(taxid))
+            return {r: str(t) for t, r in lineage.items() if r in RANKS}
+        except ValueError:
+            # This will happen if the ID is not found or the dbs are out of date
+            return {}
+
+    def get_lineage_safe(self, taxid, safe=True):
+        """
+        Wrapper around NCBI.get_lineage to return an empty list when ID isn't
+        recognised. Get the full lineage of NCBI and get a {} if ID isn't
+        recognised
+        :param taxid: NCBI Taxon ID
+        :type taxid: int or str
+        :param safe: catch ValueErrors and return empty dict
+        :type safe: bool
+        :returns dictionary of taxids that represent taxid's full lineage.
+        :raises ValueError: Taxonomic ID is not recognised when safe=False.
+        """
+        try:
+            return super().get_lineage(int(taxid))
+        except ValueError as e:
+            if safe:
+                return []
+            else:
+                raise e
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-import ncbitaxonomy
+from ncbitaxonomy.ncbi_taxonomy import ncbiquery2
 
-NCBI = ncbitaxonomy.NCBITaxa()
+NCBI = ncbiquery2.NCBITaxa()
 NCBI.update_taxonomy_database()
 print(NCBI.get_lineage("9606"))
 assert NCBI.get_lineage("9606") == [1, 131567, 2759, 33154, 33208, 6072, 33213, 33511, 7711, 89593, 7742, 7776, 117570, 117571, 8287, 1338369, 32523, 32524, 40674, 32525, 9347, 1437010, 314146, 9443, 376913, 314293, 9526, 314295, 9604, 207598, 9605, 9606]
